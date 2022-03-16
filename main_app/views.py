50642ce749c571f97be507b88911101892dda809
Denee Boyd <deneeoliviaboyd@gmail.com>
2022-03-15 23:50:52 -0500

    css styling changed to blue and gold

--- main_app/views.py
+++ main_app/views.py
@@ -48,7 +48,6 @@ class ItemList(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs) 
 
-        
         title = self.request.GET.get('title')
         print(title)
         if title != None:
